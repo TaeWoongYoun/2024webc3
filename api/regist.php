440d623995ef1367f9257902439857b47abeade8
xodnd0507 <24457545yong@gmail.com>
2024-08-03 23:02:15 +0900

    test : Update

--- api/regist.php
+++ api/regist.php
@@ -6,8 +6,8 @@ header('Content-Type: application/json');
 $userid = mysqli_real_escape_string($conn, $_POST['userid']);
 $username = mysqli_real_escape_string($conn, $_POST['username']);
 $userpassword = password_hash($_POST['userpassword'], PASSWORD_BCRYPT);
-$token = bin2hex(random_bytes(16)); // 16바이트의 랜덤 문자열을 32자리의 16진수로 변환합니다.
-$apikey = bin2hex(random_bytes(16)); // 동일하게 32자리의 API 키를 생성합니다.
+$token = ''; // 16바이트의 랜덤 문자열을 32자리의 16진수로 변환합니다.
+$apikey = ''; // 동일하게 32자리의 API 키를 생성합니다.
 
 // 사용자 데이터 삽입 SQL 쿼리
 $sql = "INSERT INTO users (role, username, userid, userpassword, token, apikey) VALUES ('u', '$username', '$userid', '$userpassword', '$token', '$apikey')";
